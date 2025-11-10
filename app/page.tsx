4c5532196a7210399b235fa4817f6d01e78a8c35
Firda Rosiana T <firdaroziana10@gmail.com>
2025-11-10 12:41:20 +0700

    update card items service-why choose

--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,17 @@ import { SiWebpack, SiWhatsapp } from "react-icons/si";
 export default function Home() {
   return (
     <div className="font-sans text-white bg-black overflow-x-hidden">
-      {/* Hero Section */}
       <section
         id="home"
         className="relative min-h-screen flex flex-col justify-center items-center text-center px-6 md:px-10 overflow-hidden"
         style={{
-          backgroundImage: `linear-gradient(rgba(16,185,129,0.05) 1px, transparent 1px),
+          backgroundImage: `linear-gradient(rgba(16,185,129,0.08) 1px, transparent 1px),
                       linear-gradient(90deg, rgba(16,185,129,0.05) 1px, transparent 1px)`,
           backgroundSize: "24px 24px",
         }}
       >
-        <div className="absolute top-0 left-1/4 w-72 h-72 bg-emerald-500/20 blur-3xl rounded-full pointer-events-none"></div>
-        <div className="absolute bottom-10 right-1/4 w-64 h-64 bg-emerald-400/20 blur-3xl rounded-full pointer-events-none"></div>
+        <div className="absolute top-0 left-1/4 w-72 h-72 bg-emerald-500/20 blur-2xl rounded-full pointer-events-none"></div>
+        <div className="absolute bottom-10 right-1/4 w-64 h-64 bg-emerald-400/20 blur-2xl rounded-full pointer-events-none"></div>
 
         <h1 className="relative z-10 text-5xl md:text-6xl  font-semibold mb-4 text-emerald-400 leading-tight">Zackstudio hadir untukmu 🚀</h1>
         <p className="relative z-10 text-xl md:text-2xl max-w-2xl mb-10 leading-relaxed">Bantu kamu ngerjain, nyelesain, dan ngerapiin proyek biar hasilnya rapi, cepat, dan siap pakai.</p>
@@ -51,7 +50,6 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Services Section */}
       <section id="services" className="py-16 px-4 md:px-10 max-w-6xl mx-auto">
         <h2 className="text-2xl md:text-3xl font-bold text-center mb-10 text-emerald-400">What We Do</h2>
 
@@ -92,8 +90,12 @@ export default function Home() {
               badge: true,
             },
           ].map((item, i) => (
-            <div key={i} className="relative p-5 rounded-2xl bg-zinc-900/50 shadow-lg hover:shadow-emerald-500/50 transition text-center border border-zinc-800 hover:-translate-y-1">
-              {/* Badge di pojok kanan atas */}
+            <div
+              key={i}
+              className="relative p-5 rounded-2xl bg-zinc-900/50 text-center border border-zinc-800 
+             transition-all duration-300 hover:-translate-y-2 hover:border-emerald-500/50 hover:bg-zinc-800/70"
+            >
+              {/* badge pojok kanan atas */}
               {item.badge && (
                 <div className="absolute top-3 right-3 bg-white/30 text-white text-xs font-bold px-2 py-1 rounded flex items-center gap-1">
                   Most Ordered <BiSolidLike />{" "}
@@ -133,7 +135,12 @@ export default function Home() {
               desc: "Bantuan sampai websitemu go live tanpa bingung pasang hosting/lokal server.",
             },
           ].map((item, i) => (
-            <div key={i} className="flex flex-col items-center gap-3 p-5 rounded-2xl bg-zinc-900/50 shadow-lg hover:shadow-emerald-500/50 transition border border-zinc-800 hover:-translate-y-1">
+            <div
+              key={i}
+              className="flex flex-col items-center gap-3 p-5 rounded-2xl bg-zinc-900/50 
+                   text-center border border-zinc-800 transition-all duration-300 
+                   hover:-translate-y-2 hover:border-emerald-500/50 hover:bg-zinc-800/70"
+            >
               <span className="text-4xl">{item.emoji}</span>
               <h3 className="text-xl font-semibold text-emerald-400">{item.title}</h3>
               <p className="text-zinc-300 text-md leading-relaxed max-w-xs mx-auto">{item.desc}</p>
